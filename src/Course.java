4bcfc62b214ef77f000e2aefc97d366e90dbb622
Kasey Dionne <kasey.dionne@unb.ca>
2018-03-19 20:57:39 -0300

    Added isAbsent and setIsAbsent methods to the Course class

--- src/Course.java
+++ src/Course.java
@@ -2,18 +2,19 @@
 
 public class Course {
 
-	
 	String courseTitle;
 	Period period;
 	Teacher regularTeacher;
+	boolean isAbsent = false;
 	
 	public Course (String courseTitle, Period period, Teacher regularTeacher){
-		
 		this.courseTitle = courseTitle;
 		this.period = period;
 		this.regularTeacher = regularTeacher;
-		
 	}
 	
+	public void setIsAbsent(boolean in) {
+		isAbsent = in;
+	}
 	
 }
