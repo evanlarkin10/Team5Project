713e7ed818e9e41f1e161f1d54c608b7123c1b05
Kasey Dionne <kasey.dionne@unb.ca>
2018-03-20 20:22:53 -0300

    resetMonthlyTally method added to ConfigWorkbook class. Config file moved out of ExcelWorkbook folder.

--- src/ConfigWorkbook.java
+++ src/ConfigWorkbook.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import jxl.*;
+import jxl.write.*;
+import jxl.write.Number;
+import jxl.write.biff.RowsExceededException;
 import jxl.read.biff.BiffException;
 public class ConfigWorkbook {
 	private Sheet masterSchedule;
@@ -11,10 +14,14 @@ public class ConfigWorkbook {
 	private Sheet absenceSchedule;
 	private Sheet tallySheet;
 	private File configFile;
+	private Workbook wb;
+	private WritableWorkbook wbWritable;
+	private WritableSheet tallySheetWritable;
+	private WritableWorkbook copyDocument;
 	
 	public ConfigWorkbook(File configFileIn) throws BiffException, IOException {
 		configFile = configFileIn;
-		Workbook wb = Workbook.getWorkbook(configFileIn);
+		wb = Workbook.getWorkbook(configFileIn);
 		masterSchedule = wb.getSheet("Master Schedule");
 		supplyList = wb.getSheet("Supply List");
 		skills = wb.getSheet("Skills");
@@ -30,74 +37,101 @@ public class ConfigWorkbook {
 		int row;
 		int next;
 		
-			column = masterSchedule.findCell("Teacher's Name").getColumn();
-			row = masterSchedule.findCell("Teacher's Name").getRow();
-			//Create Teacher Objects from the spreadsheet
-			next = 1;
-			while (! masterSchedule.getCell(column ,row + next).getContents().equals("")){
-				teachers.add(new Teacher(masterSchedule.getCell(column ,row + next).getContents()));
-				next = next + 1;
+		column = masterSchedule.findCell("Teacher's Name").getColumn();
+		row = masterSchedule.findCell("Teacher's Name").getRow();
+		//Create Teacher Objects from the spreadsheet
+		next = 1;
+		while (! masterSchedule.getCell(column ,row + next).getContents().equals("")){
+			teachers.add(new Teacher(masterSchedule.getCell(column ,row + next).getContents()));
+			next = next + 1;
+		}
+	
+		//Add courses to teachers starting with the first teacher
+		column = masterSchedule.findCell(teachers.get(0).NAME).getColumn();
+		row = masterSchedule.findCell(teachers.get(0).NAME).getRow();
+		for(Teacher teacher : teachers){
+			ArrayList<Course> courses = new ArrayList<Course>();
+			for(int i = 1; i <= numberOfPeriods; i++){
+				Period period = Period.Period1;
+				switch(i) { 
+				case 1:
+					period = Period.Period1;
+					break;
+				case 2: 
+					period = Period.Period2;
+					break;
+				case 3:
+					period = Period.Period3A;
+					break;
+				case 4:
+					period = Period.Period3B;
+					break;
+				case 5: 
+					period = Period.Period4;
+					break;	
+				}
+				courses.add(new Course(masterSchedule.getCell(column + i ,row).getContents(), period ,teacher));
 			}
+			
+			for(Course course : courses){
+				teacher.addCourse(course);
+			}
+		}
 		
-			//Add courses to teachers starting with the first teacher
-			column = masterSchedule.findCell(teachers.get(0).NAME).getColumn();
-			row = masterSchedule.findCell(teachers.get(0).NAME).getRow();
-			for(Teacher teacher : teachers){
-				ArrayList<Course> courses = new ArrayList<Course>();
-				for(int i = 1; i <= numberOfPeriods; i++){
-					Period period = Period.Period1;
-					switch(i) { 
-					case 1:
-						period = Period.Period1;
-						break;
-					case 2: 
-						period = Period.Period2;
-						break;
-					case 3:
-						period = Period.Period3A;
-						break;
-					case 4:
-						period = Period.Period3B;
-						break;
-					case 5: 
-						period = Period.Period4;
-						break;	
-					}
-					courses.add(new Course(masterSchedule.getCell(column + i ,row).getContents(), period ,teacher));
-				}
-				
-				for(Course course : courses){
-					teacher.addCourse(course);
-				}
-			}
+		//Add skills to teacher 
+		column = masterSchedule.findCell("Teachable Skill").getColumn();
+		row = masterSchedule.findCell("Teachable Skill").getRow() + 1;
+		
+		for(Teacher teacher : teachers){
+			ArrayList<String> skills = new ArrayList<String>();
+			String teacherSkill = masterSchedule.getCell(column ,row + teachers.indexOf(teacher)).getContents() ;
+			skills.add(teacherSkill);
 			
-			//Add skills to teacher 
-			column = masterSchedule.findCell("Teachable Skill").getColumn();
-			row = masterSchedule.findCell("Teachable Skill").getRow() + 1;
-			
-			for(Teacher teacher : teachers){
-				ArrayList<String> skills = new ArrayList<String>();
-				String teacherSkill = masterSchedule.getCell(column ,row + teachers.indexOf(teacher)).getContents() ;
-				skills.add(teacherSkill);
-				
-				for(String skill : skills){
-					teacher.addSkill(skill);
-				}
+			for(String skill : skills){
+				teacher.addSkill(skill);
 			}
-			
-			//Print out teachers , courses, and Period they teach
-			for(Teacher teacher : teachers){
-				System.out.println(teacher.NAME + " Skill:" + teacher.skills);
-				for(Course course : teacher.courses){
-					System.out.println(course.courseTitle + " " + course.period);
-				}
-				System.out.println();
+		}
+		
+		//Print out teachers , courses, and Period they teach
+		for(Teacher teacher : teachers){
+			System.out.println(teacher.NAME + " Skill:" + teacher.skills);
+			for(Course course : teacher.courses){
+				System.out.println(course.courseTitle + " " + course.period);
 			}
+			System.out.println();
+		}
 	}
 
-	public void resetMonthlyTally() {
+	public void resetMonthlyTally()  throws BiffException, IOException, RowsExceededException, WriteException {
+		wbWritable = Workbook.createWorkbook(new File("ConfigFile.xls"), wb);
+		tallySheetWritable = wbWritable.getSheet("Tally");	
+		
+		int column = 2;
+		int startingRow = 2;
+		int next = 0;
+		
+		while (! tallySheetWritable.getCell(column , startingRow + next).getContents().equals("")) {
+			System.out.println("Entered the while loop");
+			
+			WritableCell cell;
+			Number resetTally = new Number(column, (startingRow + next), 0);
+			cell = (WritableCell) resetTally;
+			
+			tallySheetWritable.addCell(cell);	
+			next++;
+		}
+		
+		wbWritable.write();
+		wbWritable.close();
+		
+		wb = Workbook.getWorkbook(new File("ConfigFile.xls"));
+		wb.close();
+	}
+	
+	public void copySheet() {
 		
 	}
+
 	public void resetWeeklyTally() {
 		
 	}
